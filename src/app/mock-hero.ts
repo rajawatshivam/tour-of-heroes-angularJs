e42da1fa0cb1791fd7d5b177874d963d099d0e67
Shivamrajawat <rajawatshivam007@gmail.com>
2018-08-10 14:48:28 +0530

    20% completed

---
+++ src/app/mock-hero.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+export const HEROES: Hero []=  [ 
+{ id: 11, name: 'Mr. Nice',lname:'hunt'  },
+{ id: 12, name: 'Narco', lname:'hunt' },
+{ id: 13, name: 'Bombasto',lname:'hunt' },
+{ id: 14, name: 'Celeritas',lname:'hunt'  },
+{ id: 15, name: 'Magneta',lname:'hunt'  },
+{ id: 16, name: 'RubberMan',lname:'hunt'  },
+{ id: 17, name: 'Dynama',lname:'hunt'  },
+{ id: 18, name: 'Dr IQ',lname:'hunt'  },
+{ id: 19, name: 'Magma',lname:'hunt'  },
+{ id: 20, name: 'Tornado',lname:'hunt'  }
+];
